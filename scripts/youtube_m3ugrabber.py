ff6845fdd30551a7d2ad32646287a2ccaf41aab6
naveenland4 <32125555+naveenland4@users.noreply.github.com>
2022-05-30 10:44:43 +0530

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -13,7 +13,7 @@ banner = r'''
 
 
 
-#EXTINF:-1 group-title="Test" tvg-logo="https://i.imgur.com/aESqPMs.png" tvg-id="", VDO
+#EXTINF:-1 group-title="Indo - Must Read" tvg-logo="https://i.imgur.com/aESqPMs.png" tvg-id="", Playlist is for Free
 https://raw.githubusercontent.com/naveenland4/UTLive/main/assets/info.m3u8
 #EXTINF:-1 group-title="4K" tvg-logo="https://i.imgur.com/oYYncnc.png" tvg-id="", Loupe 4K
 https://d2dw21aq0j0l5c.cloudfront.net/v1/master/3722c60a815c199d9c0ef36c5b73da68a62b09d1/LoupeArt-prod/playlist.m3u8|User-Agent=ExoPlayerLib/2.9.6
